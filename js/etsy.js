d86382890e07665a5cbe9d25c0a996f869701909
cjros <clor09@gmail.com>
2015-02-02 18:43:32 -0600

    updating

--- js/etsy.js
+++ js/etsy.js
@@ -5,10 +5,20 @@
 		//backbone routing
 		var etsyRouter = Backbone.Router.extend({
             routes: {
-                ":listing_id": "getListingDetails"
+                ":listing_id": "getListingDetails",
+                '?keywords=:keywords': "search" 
+
             },
-            getListingDetails: function(listing_id){
+            getListingDetails: function(listing_id) {
                 self.showDetailedView(listing_id)
+            },
+            search: function(keywords) {
+            	$(document).ready(function() {
+	            	$("#form").on('submit', function(event) {
+	            		event.preventDefault();
+	            	})
+	            })
+            	//self.startSearch(keywords);
             },
             initialize: function(){
                 Backbone.history.start();
@@ -21,7 +31,7 @@
 
 	Etsy.prototype = {
 		//learn to fucking write promises...
-		//page loading like dial-up...
+		//page loading like dial-up connection
 		getFeaturedData: function() {
 			return $.getJSON("https://openapi.etsy.com/v2/featured_treasuries/listings.js?includes=Images:1&callback=?&api_key=v8b5h6fqelovdop2ja8usrgm")
 			.then(function(d, s, p) {
@@ -32,6 +42,12 @@
 		},
 		getDetailedData: function(listing_id) {
 			return $.getJSON("https://openapi.etsy.com/v2/listings/" + listing_id + ".js?includes=Images:1&callback=?&api_key=v8b5h6fqelovdop2ja8usrgm")
+			.then(function(d, s, p) {
+				return d.results;
+			});
+		},
+		dataSearchListings: function(keywords) {
+			return $.getJSON("https://openapi.etsy.com/v2/listings/active.js?keywords=" + keywords + "&includes=Images:1&api_key=v8b5h6fqelovdop2ja8usrgm")
 			.then(function(d, s, p) {
 				return d.results;
 			});
@@ -62,6 +78,24 @@
 					var deets = document.querySelector(".detailer");
 					deets.innerHTML = temp2( {detail_info: detail_info[0]} );
 				})
+		},
+		startSearch: function(keywords) {
+			$.when(
+				this.loadTemplate("items"),
+				this.dataSearchListings(keywords)
+				).then(function(html, searches) {
+					var temp3 = _.template(html);
+					var results = document.querySelector(".results");
+					results.innerHTML = temp3( {searches: searches} );
+				})
+			// var search = [];
+			// var terms = search.push(document.querySelector("form").value);
+			//on.click
+			//put keywords in array then .join(" ")
+			//feed through here
+			//look for a match in /listing/ through key: "keywords"
+			//display through innerHTML
+			
 		}
 	}
 
